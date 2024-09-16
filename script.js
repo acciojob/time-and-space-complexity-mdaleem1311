0d0f5444cfc44c8e5ba1a1c2d9c811ac1380143b
mdaleem1311 <mdaleem1311@gmail.com>
2024-09-16 16:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 //your JS code here. If required.
-keywordArray.forEach(keyword => {
-  cy.get(tag).eq(index)
-    .invoke('text')
-    .then((text) => {
-      expect(text.toLowerCase()).to.include(keyword.toLowerCase());
-    });
-});
